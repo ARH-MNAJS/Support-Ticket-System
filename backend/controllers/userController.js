6764afd3679716590368c4c54e71dcdb4052485d
ARHMNAJS <aditya.singh20@vit.edu.in>
2025-01-03 09:42:15 +0530

    Authorization Middlewares done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,8 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/userModel");
 
 const registerRoute = asyncHandler( async (req, res) => {
     const { name, email, password } = req.body;
@@ -7,14 +11,59 @@ const registerRoute = asyncHandler( async (req, res) => {
         res.status(400)
         throw new Error("Please fill all fields");
     }
-    res.send("Register a user");
+
+    const userExists = await User.findOne({email})
+    if (userExists) {
+        res.status(400)
+        throw new Error("User already exists");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "30d"})
+        })
+    } else {
+        res.status(400)
+        throw new Error("Invalid user data");
+    }
 })
 
 const loginRoute = asyncHandler( async (req, res) => {
-    res.send("Login a user");
+    const { email, password } = req.body;
+
+    const user = await User.findOne({email});
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "30d"})
+        })
+    } else {
+        res.status(401)
+        throw new Error("Invalid Credentials");
+    }
+})
+
+const getMe = asyncHandler( async (req, res) => {
+    res.send('me')
 })
 
 module.exports = {
     registerRoute,
     loginRoute,
+    getMe
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { loginRoute, registerRoute } = require('../controllers/userController');
+const { loginRoute, registerRoute, getMe } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 
 router.post('/', registerRoute);
 router.post('/login', loginRoute);
+router.get('/me', protect, getMe);
 
 module.exports = router;
